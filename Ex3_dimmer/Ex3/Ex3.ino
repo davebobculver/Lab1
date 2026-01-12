816c70a6da298b0064a05db1a783d21369f05c86
David Culver <david.culver94@gmail.com>
2026-01-12 13:46:42 -0800

    Ex3 is complete

--- Ex3_dimmer/Ex3/Ex3.ino
+++ Ex3_dimmer/Ex3/Ex3.ino
@@ -10,6 +10,7 @@ void setup() {
 
 
 float outval = 0; unsigned long tbut = 0; unsigned long tnow = 0; unsigned long tdiff = 0;
+int dim = HIGH; int off = LOW;
 int state = LOW;
 int lastval = LOW;
 int val = LOW;
@@ -23,10 +24,15 @@ if (lastval == LOW && val == HIGH){// Button Press
 tbut = millis();
 Serial.print("Button press at ");
 Serial.println(tbut);
+
+if (off == HIGH){ // Turn of LED if in off mode
+digitalWrite(LED,LOW); //Turns off LED  if secodn let go of button
+}
+
 delay(bl);
 }
 
-if (val == HIGH){
+if (val == HIGH && dim == HIGH){
 if (outval >= 255){outval =255;}
 outval += .005;
 analogWrite(LED, ((int) (outval)));
@@ -35,8 +41,6 @@ analogWrite(LED, ((int) (outval)));
 
 
 if (lastval == HIGH && val == LOW){// start turn off if statement
-state = LOW;
-digitalWrite(LED, state); //TURN off LED
 
 tnow = millis();
 tdiff = tnow-tbut;
@@ -46,10 +50,20 @@ Serial.print("Brightness of ");
 Serial.println((int) outval);
 Serial.print("Button off at ");
 Serial.println(tnow);
-
 delay(bl); // Switch Bounce
 
-outval = 0;
+
+if (off == HIGH){
+off = LOW;
+dim = HIGH;
+}
+
+else if (dim == HIGH){ //Switch if on dimming mode or turning off mode
+off = HIGH;
+dim = LOW;
+outval =0;
+}
+
 }//End switch off if statement
 
 lastval = val;
