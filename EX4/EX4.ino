15f2e5d700472b75babd1e605eaab9a942bcfaee
davebobculver <david.culver94@gmail.com>
2026-01-12 15:07:56 -0800

    Updated Ex 4

--- EX4/EX4.ino
+++ EX4/EX4.ino
@@ -1,5 +1,5 @@
 #define RES A5
-#define LED 11
+#define LED 3
 
 void setup() {
   Serial.begin(9600);
@@ -8,13 +8,21 @@ void setup() {
 
 }
 float res = 0;
+int res_print = 0;
+
 void loop() {
   // put your main code here, to run repeatedly:
 res = analogRead(RES); // Read the resistivity
+
+if (res_print% 5000 == 0) {
 Serial.print("RES ");
 Serial.println((int) res); // print current state of resistance
+}
 
-res = res/4; // Make blink with brightnes
+res = (res/1024)*255; // scale analog out to 0-255
+if (res <= 0 ){res = 0;}
 analogWrite(LED, (int) res);
+
+res_print += 1;
 
 }
